27f55b8cd7f50dd1c2cc4f226d4e8a6e1a76853c
JosafatLaguna <159727696+JosafatLaguna@users.noreply.github.com>
2024-10-09 19:24:24 -0600

    Add files via upload

---
+++ practica1/src/main/java/com/mycompany/practica1/Practica1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aulas Heredia
+ */
+public class Practica1 {
+
+    public static void main(String[] args) {
+        
+        String empleados = JOptionPane.showInputDialog("Ingrese la cantidad de empleados:");
+        double FyM = 0.925;
+        double VyM = 0.58;
+        
+        int i = 0;
+            for (int i = 1; i <= empleados; i++){
+                
+            }
+        
+            
+                
+        
+    }
+            
+                
+}
